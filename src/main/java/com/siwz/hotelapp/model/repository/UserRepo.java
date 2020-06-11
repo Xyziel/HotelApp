43527915adc9b5911973ec9768c5c2ce4cb615d6
Darboux12 <dariuszbula98@gmail.com>
2020-06-11 20:09:40 +0200

    Adding user queries

--- src/main/java/com/siwz/hotelapp/model/repository/UserRepo.java
+++ src/main/java/com/siwz/hotelapp/model/repository/UserRepo.java
@@ -1,9 +1,23 @@
 package com.siwz.hotelapp.model.repository;
 
+import com.siwz.hotelapp.model.entity.Role;
 import com.siwz.hotelapp.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User,Integer>
-{
+import java.util.List;
+
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+    List<User> findByUserName(String userName);
+
+    List<User> findByFirstName(String firstName);
+
+    List<User> findByLastName(String lastName);
+
+    List<User> findByEmail(String email);
+
+    List<User> findByPhoneNumber(String phoneNumber);
+
+    List<User> findByRole(Role role);
 
 }
