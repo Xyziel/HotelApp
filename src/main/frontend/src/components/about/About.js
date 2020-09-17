d5a35a49467dba3d5fbd70fe77215660fd04c3f9
Darboux12 <dariuszbula98@gmail.com>
2020-09-17 20:36:36 +0200

    Main Page About

--- src/main/frontend/src/components/about/About.js
+++ src/main/frontend/src/components/about/About.js
@@ -2,20 +2,61 @@ import React from "react";
 import '../../styles/css/about/About.css';
 import {Container} from "react-bootstrap";
 
+import HotelView from "../../styles/img/HotelView.jpg"
+import Personnel from "../../styles/img/personnel.jpg"
+import Room from "../../styles/img/rooms/room100.jpg"
+
 class About extends React.Component {
 
     render() {
         return (
             <Container>
-                <h2>Cos o hotelu</h2>
+                <h2 className={"mb-5 aboutHeader"}>About our hotel...</h2>
+
                 <div className="info">
                     <div className="column">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p className={"aboutParagraph"}>Our Hotel is located only 100 m from Aya Napa beach,
+                        one of the most beautiful beaches on the Cyprus. <br/><br/>
+                            Clean and clear water, yellow sand and hot temperatures. All of it and much more waits for you!  </p>
                     </div>
-                    <div className="column">
-                        <h3>Tu może być zdjęcie</h3>
+                    <div className="column mb-5">
+                        <img className={"w-100 ml-5"} src={HotelView} alt={"Hotel View Image"}/>
                     </div>
                 </div>
+
+                <div className="info">
+                    <div className="column">
+                        <p className={"aboutParagraph"}>
+                            Our personnel will do their best to provide you everthing
+                            you need and much more!
+                            <br/><br/>
+                            Delicious meals, cold local and export drinks served in the pool
+                            by our experienced people will make our holidays unforgettable!
+                        </p>
+                    </div>
+                    <div className="column mb-5">
+                        <img className={"w-100 ml-5"} src={Personnel} alt={"Hotel View Image"}/>
+                    </div>
+                </div>
+
+                <div className="info">
+                    <div className="column">
+                        <p className={"aboutParagraph"}>
+
+                            ALl of our room was renowned in 2019.
+                            All of them include, frideg, shower and air conditioning.
+
+                            <br/><br/>
+                           Our every room is cleaned every day and
+                            you are getting fresh towels.
+                        </p>
+                    </div>
+                    <div className="column mb-5">
+                        <img className={"w-100 ml-5"} src={Room} alt={"Hotel View Image"}/>
+                    </div>
+                </div>
+
+
             </Container>
         )
     }
--- src/main/frontend/src/components/main/Slider.js
+++ src/main/frontend/src/components/main/Slider.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Carousel, Container} from 'react-bootstrap/esm/index';
 
+import "../../styles/css/main/Slider.css"
+
 class Slider extends React.Component {
 
     render() {
         return (
             <Container>
-                <Carousel fade="true">
+                <Carousel interval="4000" data-pause="false" pause="false">
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
@@ -14,8 +16,8 @@ class Slider extends React.Component {
                             alt="cos tam"
                         />
                         <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3 className={"SliderHeader"}>Be our guest</h3>
+                            <p className={"SliderParagraph"}>Professional personnel, modern rooms, beautiful view, this is what our hotel is about.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
@@ -25,8 +27,8 @@ class Slider extends React.Component {
                             alt="cos tam"
                         />
                         <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3 className={"SliderTwoHeader"}>Take a rest from big city</h3>
+                            <p className={"SliderTwoParagraph"}>Our hotel is located in quiet place in the forest. Only trees and birds singing.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
@@ -36,8 +38,8 @@ class Slider extends React.Component {
                             alt="cos tam"
                         />
                         <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3 className={"SliderHeader"}>Dive into luxury </h3>
+                            <p className={"SliderParagraph"}>Our rooms please event the most demanding guests. </p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
